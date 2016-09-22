3128733b17626d76c7736ad3a5f8ade0bcd6f3a8
clebsonc <clebsondm@gmail.com>
2016-09-21 21:29:46 -0300

    insertion_sort2 working

--- sorting/insertion_sort2.cpp
+++ sorting/insertion_sort2.cpp
@@ -9,19 +9,17 @@ void print_array(const int * const p, const int n){
 }
 
 void insertion_sort(int *p, int n){
-  int j=0;
-  int temp=0;
-  for(int i=0; i<n; i++){
+  int j = 0, temp = 0;
+  for(int i = 1; i < n; i++){
     j = i;
-    temp = *(p+i);
-    while(j > 0 && *(p+j-1) >= temp){
-      *(p+j) = *(p+j-1);
+    temp = p[i];
+    while(j>0 && p[j-1] > temp){
+      p[j] = p[j-1];
       j--;
     }
-    if(j!=i){
-      p[j] = temp;
-      print_array(p, n);
-    }
+    p[j] = temp;
+    //std::cout << "(i,j) = (" <<  i << ", " << j << ")" << std::endl;
+    print_array(p, n);
   }
 }
 
