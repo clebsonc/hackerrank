57978a7196026cfdcb2defee1d2d6d79fb9e5ae8
clebsonc <clebsondm@gmail.com>
2017-03-25 13:46:26 -0300

    running median working

--- ctci_tutorial/find_the_running_median.cpp
+++ ctci_tutorial/find_the_running_median.cpp
@@ -2,6 +2,8 @@
 #include <algorithm>
 #include <vector>
 #include <queue>
+#include <iomanip>
+
 
 struct comparator{
   bool operator()(const int & a, const int & b){
@@ -17,12 +19,41 @@ void print_queue(std::priority_queue<int, std::vector<int>, comparator> pq){
   std::cout << std::endl;
 }
 
-void read_values(std::priority_queue<int, std::vector<int>, comparator> & pq, const int n){
+void transfer_elements_queue(std::priority_queue<int> & max_pq, 
+    std::priority_queue<int, std::vector<int>, comparator> & min_pq,
+    int qt_elements){
+  for (int i = 0; i <= qt_elements; i++){
+    int value = max_pq.top();
+    max_pq.pop();
+    min_pq.emplace(value);
+  }
+}
+
+void print_median(std::priority_queue<int> max_pq){
+  std::priority_queue<int, std::vector<int>, comparator> min_pq;
+  int mid = max_pq.size()/2;
+  bool odd = max_pq.size() % 2 ? true : false;
+  float median = 0.0;
+  if (odd){
+    transfer_elements_queue(max_pq, min_pq, mid);
+    median = min_pq.top();
+  } 
+  else{  // it's an even number
+    transfer_elements_queue(max_pq, min_pq, mid-1);
+    median = (max_pq.top() + min_pq.top()) / 2.;
+  }
+  std::cout << std::fixed << std::setprecision(1);
+  std::cout << median << std::endl;
+}
+
+
+void read_values(const int n){
+  std::priority_queue<int> max_pq;
   int value;
   for (int i = 0; i < n; i++){
     std::cin >> value;
-    pq.emplace(value);
-    print_queue(pq);
+    max_pq.emplace(value);
+    print_median(max_pq);
   }
 }
 
@@ -30,8 +61,7 @@ void read_values(std::priority_queue<int, std::vector<int>, comparator> & pq, co
 int main(){
   int n = 0;
   std::cin >> n;
-  std::priority_queue<int, std::vector<int>, comparator> pq;
-  read_values(pq, n);
+  read_values(n);
 
   return 0;
 }
