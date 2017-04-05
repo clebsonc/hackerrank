cefc115691f18f45e898e8708a326ee9433035a5
clebsonc <clebsondm@gmail.com>
2017-04-04 23:06:43 -0300

    working, but needs improvemnt when counting the elements

--- ctci_tutorial/tries_contacts.cpp
+++ ctci_tutorial/tries_contacts.cpp
@@ -5,29 +5,89 @@
 
 class Node{
   public:
-    std::unordered_map<char, Node*> values;
+    std::unordered_map<char, Node*> hash;
     bool end_word;
       
-    Node():end_word{true}{}
+    Node():end_word{false}{}
+
+    ~Node(){
+      for (auto it = hash.begin(); it != hash.end(); it++){
+        delete it->second;
+      }
+    }
+
 };
 
 
 class Trie{
   private:
     Node root;
-    Node * current;
-
   public:
     void add_word(const std::string & s){
-      for (char c : s){
-        if (root.
+      Node * aux = &root;
+      for (int i=0; i < s.size(); i++){
+        char c = s.at(i);
+        std::unordered_map<char, Node*>::iterator it = aux->hash.find(c);
+        if (it != aux->hash.end()){
+          aux = it->second;
+        }
+        else {
+          aux->hash.emplace(c, new Node());
+          it = aux->hash.find(c);
+          aux = it->second;
+        }
+        if (i==s.size()-1){
+          aux->end_word = true;
+        }
       }
+    }
+    
+    void count_end_words(Node * aux, int & counter){
+      if (aux->hash.empty()){
+        return;
+      }
+      
+      for(auto it = aux->hash.begin(); it!= aux->hash.end(); it++){
+        if (it->second->end_word)
+          counter++;
+        count_end_words(it->second, counter);
+      }
+    }
+
+    int count_words_starting_with(const std::string &s){
+      Node * aux = &root;
+      for (const char c : s){
+        std::unordered_map<char, Node*>::iterator it = aux->hash.find(c);
+        if (it == aux->hash.end()){
+          /**letter does not exist in the current branch, therefore, 
+          * the word does not exist in the trie */
+          return 0;
+        }
+        aux = it->second;
+      }
+      int count = 0;
+      if (aux->end_word)
+        count++;
+      count_end_words(aux, count);
+      return count;
     }
 };
 
 int main(){
-  Trie a;
-  a.add_word("abc");
+  int n;
+  std::cin >> n;
+  Trie trie;
+  for(int i = 0; i < n; i++){
+    std::string s;
+    std::cin >> s;
+    if(!s.compare("add")){
+      std::cin >> s;
+      trie.add_word(s);
+    } else {
+      std::cin >> s;
+      std::cout << trie.count_words_starting_with(s) << std::endl;
+    }
+  }
 
   return 0;
 }
